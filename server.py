938813d3a1cd96d54bc076ddc95ec2e374fd37d0
gss897152-pixel <gss897152@gmail.com>
2026-04-21 19:37:33 +0800

    Implement the PUT/GET/READ of TupleSpace

--- server.py
+++ server.py
@@ -1,6 +1,28 @@
 import socket
 import threading
 
+class TupleSpace:
+    def __init__(self):
+        self.tuple_space = {}
+
+    def put(self, key, value):
+        if key in self.tuple_space:
+            return "024 ERR key already exists"
+        self.tuple_space[key] = value
+        return f"0{len(key)+len(value)+14} OK ({key},{value}) added"
+
+    def get(self, key):
+        if key not in self.tuple_space:
+            return "024 ERR key does not exist"
+        val = self.tuple_space.pop(key)
+        return f"0{len(key)+len(val)+16} OK ({key},{val}) removed"
+
+    def read(self, key):
+        if key not in self.tuple_space:
+            return "024 ERR key does not exist"
+        val = self.tuple_space[key]
+        return f"0{len(key)+len(val)+14} OK ({key},{val}) read"
+
 def start_server():
     host = 'localhost'
     port = 51234
